243065ab1a1baf4eb30a35ecf31cea22b507f1f7
Dylan Moon <moontruggy@gmail.com>
2022-12-02 17:01:14 -0800

    added null coalescing

--- GitMunnyApi/Filters/TransactionFilters/TransactionFromVendor.cs
+++ GitMunnyApi/Filters/TransactionFilters/TransactionFromVendor.cs
@@ -5,6 +5,6 @@ public class TransactionFromVendor : IApiFilter<TransactionModel>
     private string Value { get; }
 
     public Func<TransactionModel, bool> Filter =>
-        model => model.Vendor.Equals(Value, StringComparison.InvariantCultureIgnoreCase);
+        model => model.Vendor?.Equals(Value, StringComparison.InvariantCultureIgnoreCase) ?? false;
     public TransactionFromVendor(string value) => Value = value;
 }
